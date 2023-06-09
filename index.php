bdc3ab901ab2eb301e6a0f80ea922be2d169d408
sekisakura <s2142058.kj@s.chibakoudai.jp>
2023-06-09 14:50:12 +0900

    a

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="1.css">
+    <title>シフト管理者画面 </title>
+</head>
+
+<body>
+    <h1 style="text-align:center">シフト作成者画面</h1>
+
+    <br>
+    <a href="1.php" class="btn_01">シフト作成・登録</a>
+    <br>
+    <a href="6.php" class="btn_02">アルバイト個人情報<br>登録・消去</a>
+
+
+</body>
+
+</html>
