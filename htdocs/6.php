55e08769b8c18e2e8e0c56e935c303252d2cdcaa
井野駿也 <s2142012PV@s.chibakoudai.jp>
2023-06-09 17:15:11 +0900

    a

--- htdocs/6.php
+++ htdocs/6.php
@@ -7,7 +7,7 @@
 <body>
     <form action="6.1.php" method="post">
        <h2>アルバイト個人情報ログイン</h2>
-       <h1 style="text-align:center"> 名前をセレクトボタンで選択し<br>
+       <h2 style="text-align:center"> 名前をセレクトボタンで選択し<br>
        電話番号を入力してください</p>
        <?php if (isset($_GET['error'])){ ?>
             <p class="error"><?php echo $_GET['error']; ?></p>
