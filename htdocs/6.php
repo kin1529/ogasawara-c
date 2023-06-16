ab4ae1fc516d9842e2bea1c39659b73fc46992c0
井野駿也 <s2142012PV@s.chibakoudai.jp>
2023-06-16 16:29:58 +0900

    a

--- htdocs/6.php
+++ htdocs/6.php
@@ -22,3 +22,17 @@
     </from>
 </body>
 </html>
+
+<?php
+
+$sname= "localhost";
+$unmae= "root";
+$bangou= "";
+
+$db_name = "arubaito_db";
+
+$conn = mysqli_connect($sname, $unmae, $bangou, $db_name);
+
+if (!$conn) {
+    echo "Connection failed!";
+}
