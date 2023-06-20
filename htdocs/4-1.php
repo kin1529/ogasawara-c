a29c16e7a5d21838cdbd99a1098b434a53ba015c
onta <s2142115fw@s.chibakoudai.jp>
2023-06-20 14:58:38 +0900

    4-1

--- htdocs/4-1.php
+++ htdocs/4-1.php
@@ -1,84 +1,25 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="4.css">
-    <title>アルバイト情報　登録・消去</title>
-
+<meta charset="UTF-8" />
+<link rel='stylesheet' href='style.css' />
+<title>アルバイト情報</title>
 </head>
-
 <body>
-    <div style="text-align: center;">
-        <h1>アルバイト情報 登録・消去</h1>
-<form action="4-1.php" method="post">
-    <label for="name" style="font-size: 30px;">名前:</label>
-    <input name="name" id="name" style="font-size: 30px;">
-    <br>
-    
-    <label for="phone" style="font-size: 30px;">電話番号:</label>
-    <input type="tel" name="phone" id="phone" required style="font-size: 30px;">
-    <br>
-
-    <label for="hourly-rate" style="font-size: 30px;">時給:</label>
-    <input type="number" name="hourly-rate" id="hourly-rate" required style="font-size: 30px;">
-    <br>
-
-    <input type="submit" name="register" value="登録" class="btn_01">
-    <input type="submit" name="delete" value="消去" class="btn_02">
-</form>
-    </div>
-</body>
-
-</html>
 <?php
-// データベース接続設定
-$dbServer = '127.0.0.1';
-$dbName = 'mydb';
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
-$dbUser = 'root';
-$dbPass = '';
-// データベースへの接続
-$db = new PDO($dsn, $dbUser, $dbPass);
+# 送信されたデータの取得
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$hourlyRate = $_POST['hourly-rate'];
+require 'db.php'; # 接続
 
-// 登録ボタンが押された場合の処理
-if (isset($_POST['register'])) {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $hourlyRate = $_POST['hourly-rate'];
-
-    // プリペアドステートメントの準備
-    $stmt = $db->prepare("INSERT INTO arubaito_table (バイトID, 名前, 電話番号, 時給) VALUES (NULL, ?, ?, ?)");
-    $stmt->bindParam(1, $name);
-    $stmt->bindParam(2, $phone);
-    $stmt->bindParam(3, $hourlyRate);
-
-    // プリペアドステートメントの実行
-    if ($stmt->execute()) {
-        echo "アルバイト情報が登録されました。";
-    } else {
-        echo "アルバイト情報の登録に失敗しました。";
-    }
-}
-
-// 消去ボタンが押された場合の処理
-if (isset($_POST['delete'])) {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-
-    // プリペアドステートメントの準備
-    $stmt = $db->prepare("DELETE FROM arubaito_table WHERE 名前=? AND 電話番号=?");
-    $stmt->bindParam(1, $name);
-    $stmt->bindParam(2, $phone);
-
-    // プリペアドステートメントの実行
-    if ($stmt->execute()) {
-        echo "アルバイト情報が削除されました。";
-    } else {
-        echo "アルバイト情報の削除に失敗しました。";
-    }
-}
-
-// データベース接続を閉じる
-$db = null;
+$sql = 'insert into arubaito_table (productname, price, stock) values (:name, :phone, :hourlyRate)’;
+$prepare = $db->prepare($sql); # 準備
+$prepare->bindValue(':name', $name, PDO::PARAM_STR); # 埋め込み1
+$prepare->bindValue(':phone', $phone, PDO::PARAM_STR); # 埋め込み2
+$prepare->bindValue(':hourlyRate', $hourlyRate, PDO::PARAM_STR); # 埋め込み3
+$prepare->execute(); # 実行
 ?>
+<p><a href="show-all.php">確認</a></p>
+</body>
+</html>
