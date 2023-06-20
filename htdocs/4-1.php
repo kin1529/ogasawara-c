ad217d04d588ca0545509a507152e4a1936b2d11
onta <s2142115fw@s.chibakoudai.jp>
2023-06-20 15:39:34 +0900

    a

--- htdocs/4-1.php
+++ htdocs/4-1.php
@@ -6,21 +6,23 @@
 <title>アルバイト情報</title>
 </head>
 <body>
+
 <?php
 # 送信されたデータの取得
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $hourlyRate = $_POST['hourly-rate'];
 
-require 'mydb.sql'; # 接続
-$sql = 'insert into arubaito_table (name,phone,hourly-rate) values (:name, :phone, :hourlyRate)’;
-$prepare = $db->prepare($sql); # 準備
+require '4.php'; # 接続
+$sql = 'insert into arubaito_table (name,phone,hourly-rate) values (:name, :phone, :hourlyRate)';
+$prepare = $4->prepare($sql); # 準備
 $prepare->bindValue(':name', $name, PDO::PARAM_STR); # 埋め込み1
 $prepare->bindValue(':phone', $phone, PDO::PARAM_STR); # 埋め込み2
-$prepare->bindValue(':hourlyRate', $hosurlyRate, PDO::PARAM_STR); # 埋め込み3
+$prepare->bindValue(':hourlyRate', $hourlyRate, PDO::PARAM_STR); # 埋め込み3
 $prepare->execute(); # 実行
 ?>
 <br>
+
 <a href="1.php" class="btn_01">
 <span class="vertical-text">戻る</span>
 </a>
