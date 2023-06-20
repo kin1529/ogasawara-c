4e5d4f14110a64b86e937f7fb7b3be491ba81e22
onta <s2142115fw@s.chibakoudai.jp>
2023-06-20 15:52:03 +0900

    a

--- htdocs/4-1.php
+++ htdocs/4-1.php
@@ -1,31 +1,37 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-<meta charset="UTF-8" />
-<link rel='stylesheet' href='4-1.css' />
-<title>アルバイト情報</title>
-</head>
-<body>
-
+  <head>
+    <meta charset="UTF-8" />
+    <link rel='stylesheet' href='4-1.css' />
+    <title>アルバイトデータ表示</title>
+  </head>
+  <body>
+<table border="1">
+<tr>
+<th>名前</th>
+<th>電話番号</th>
+<th>自給</th>
+</tr>
 <?php
-# 送信されたデータの取得
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$hourlyRate = $_POST['hourly-rate'];
+require '4.php';                                                              # 接続
+$sql = 'SELECT * FROM arubaito_table';                           # SQL文
+$prepare = $db->prepare($sql);                                   # 準備
+$prepare->execute();                                                      # 実行
+$result = $prepare->fetchAll(PDO::FETCH_ASSOC);  # 結果の取得
 
-require '4.php'; # 接続
-$sql = 'insert into arubaito_table (name,phone,hourly-rate) values (:name, :phone, :hourlyRate)';
-$prepare = $db->prepare($sql); # 準備
-$prepare->bindValue(':name', $name, PDO::PARAM_STR); # 埋め込み1
-$prepare->bindValue(':phone', $phone, PDO::PARAM_STR); # 埋め込み2
-$prepare->bindValue(':hourlyRate', $hourlyRate, PDO::PARAM_STR); # 埋め込み3
-$prepare->execute(); # 実行
+foreach ($result as $row) {
+  $name = h($row['name']);
+  $phone = h($row['phone']);
+  $hourlyRate  = h($row['hourly-rate']);
+  echo "<tr><td>{$name}</td><td>{$phone}</td><td>{$hourlyRate}</td></tr>";
+}
 ?>
-<br>
+</table>
 
+<br>
 <a href="1.php" class="btn_01">
-<span class="vertical-text">戻る</span>
+  <span class="vertical-text">戻る</span>
 </a>
 
-</body>
+  </body>
 </html>
