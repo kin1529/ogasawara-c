8ee1f68103820bd3ce85716c895d41bafaabf047
sekisakura <s2142058.kj@s.chibakoudai.jp>
2023-06-20 16:31:50 +0900

    s

--- htdocs/4-1.php
+++ htdocs/4-1.php
@@ -6,33 +6,43 @@
     <title>アルバイトデータ表示</title>
   </head>
   <body>
-<table border="1">
-<tr>
-<th>名前</th>
-<th>電話番号</th>
-<th>自給</th>
-</tr>
+    <table border="1">
+      <tr>
+        <th>名前</th>
+        <th>電話番号</th>
+        <th>時給</th>
+      </tr>
 
-<?php                                                        # 接続
-$sql = 'SELECT * FROM arubaito_table';                           # SQL文
-$prepare = $db->prepare($sql);                                   # 準備
-$prepare->execute();                                                      # 実行
-$result = $prepare->fetchAll(PDO::FETCH_ASSOC);  # 結果の取得
-require '4.php'; 
-foreach ($result as $row) {
-  $name = h($row['name']);
-  $phone = h($row['phone']);
-  $hourlyRate  = h($row['hourly-rate']);
-  echo "<tr><td>{$name}</td><td>{$phone}</td><td>{$hourlyRate}</td></tr>";
-}
-?>
+      <?php
+      // データベース接続設定
+      $dbServer = '127.0.0.1';
+      $dbName = 'mydb';
+      $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+      $dbUser = 'root';
+      $dbPass = '';
+      // データベースへの接続
+      $db = new PDO($dsn, $dbUser, $dbPass);
 
-</table>
+      // アルバイト情報の取得
+      $sql = 'SELECT * FROM arubaito_table';
+      $stmt = $db->prepare($sql);
+      $stmt->execute();
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-<br>
-<a href="1.php" class="btn_01">
-  <span class="vertical-text">戻る</span>
-</a>
+      foreach ($result as $row) {
+        $name = htmlspecialchars($row['名前'], ENT_QUOTES, 'UTF-8');
+        $phone = htmlspecialchars($row['電話番号'], ENT_QUOTES, 'UTF-8');
+        $hourlyRate = htmlspecialchars($row['時給'], ENT_QUOTES, 'UTF-8');
+        echo "<tr><td>{$name}</td><td>{$phone}</td><td>{$hourlyRate}</td></tr>";
+      }
+      ?>
+
+    </table>
+
+    <br>
+    <a href="1.php" class="btn_01">
+      <span class="vertical-text">戻る</span>
+    </a>
 
   </body>
 </html>
