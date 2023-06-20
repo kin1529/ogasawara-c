c1a6cd697d9c28c76d6cd458af1baa494bacf7cb
onta <s2142115fw@s.chibakoudai.jp>
2023-06-20 15:24:33 +0900

    4-1

--- htdocs/4-1.php
+++ htdocs/4-1.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8" />
-<link rel='stylesheet' href='style.css' />
+<link rel='stylesheet' href='4-1.css' />
 <title>アルバイト情報</title>
 </head>
 <body>
@@ -11,13 +11,13 @@
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $hourlyRate = $_POST['hourly-rate'];
-require 'db.php'; # 接続
 
-$sql = 'insert into arubaito_table (productname, price, stock) values (:name, :phone, :hourlyRate)’;
+require 'mydb.sql'; # 接続
+$sql = 'insert into arubaito_table (name,phone,`hourly-rate`) values (:name, :phone, :hourlyRate)’;
 $prepare = $db->prepare($sql); # 準備
 $prepare->bindValue(':name', $name, PDO::PARAM_STR); # 埋め込み1
 $prepare->bindValue(':phone', $phone, PDO::PARAM_STR); # 埋め込み2
-$prepare->bindValue(':hourlyRate', $hourlyRate, PDO::PARAM_STR); # 埋め込み3
+$prepare->bindValue(':hourlyRate', $hosurlyRate, PDO::PARAM_STR); # 埋め込み3
 $prepare->execute(); # 実行
 ?>
 <p><a href="show-all.php">確認</a></p>
