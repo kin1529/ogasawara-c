311f39ca5d7ab224c9a093c10d901eee5b82659e
井野駿也 <s2142012PV@s.chibakoudai.jp>
2023-06-23 19:01:37 +0900

    データベース

--- htdocs/6.php
+++ htdocs/6.php
@@ -19,3 +19,46 @@
     </from>
 </body>
 </html>
+
+
+<?php
+session_start();
+include "db.php";
+
+if (isset($_POST['uname']) && isset($_POST['bangou'])){
+    
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $uname = validate($_POST['uname']);
+    $pass = validate($_POST['bangou']);
+
+    if (empty($uname)){
+        header("location: 6.php?error=正しく入力されませんでした");
+        exit();
+    }else if(empty($pass)){
+        header("location: 6.php?error=正しく入力されませんでした");
+        exit();
+    }else{
+        $sql = "SELECT * FROM users WHERE user_name='$uname' AND bangou='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($result) === 1){
+            $row = mysqli_fetch_assoc($result);
+            if ($row['user_name'] === $uname && $row['bangou'] === $pass) {
+                echo "成功";
+        }else{
+            header("location: 6.php?error=成功");
+        exit();
+        }
+}else{
+    header("location: 6.php?error=正しく入力されませんでした");
+    exit();
+}
+}
+}
