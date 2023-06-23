bad0527de970b00f511648e72994ae55de4665cf
sekisakura <s2142058.kj@s.chibakoudai.jp>
2023-06-23 18:34:38 +0900

    ok

--- htdocs/4-1.php
+++ htdocs/4-1.php
@@ -17,44 +17,41 @@
       // データベース接続設定
       require_once ("db.php");
 
-// 登録ボタンが押された場合の処理
-if (isset($_POST['register'])) {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $hourlyRate = $_POST['hourly-rate'];
+      // 登録ボタンが押された場合の処理
+      if (isset($_POST['register'])) {
+          $name = $_POST['name'];
+          $phone = $_POST['phone'];
+          $hourlyRate = $_POST['hourly-rate'];
 
-    // プリペアドステートメントの準備
-    $sql="INSERT INTO arubaito_table (バイトID, 名前, 電話番号, 時給) VALUES (?,?,:phone,:hourlyRate)";
-    $stmt = $db->prepare($sql) ;
+          // プリペアドステートメントの準備
+          $sql="INSERT INTO arubaito_table (バイトID, 名前, 電話番号, 時給) VALUES (NULL, ?, ?, ?)";
+          $stmt = $db->prepare($sql) ;
 
-    // プリペアドステートメントの実行
-    if ($stmt->execute()) {
-        echo "アルバイト情報が登録されました。";
-    } else {
-        echo "アルバイト情報の登録に失敗しました。";
-    }
-}
+          // プリペアドステートメントの実行
+          if ($stmt->execute([$name, $phone, $hourlyRate])) {
+              echo "アルバイト情報が登録されました。";
+          } else {
+              echo "アルバイト情報の登録に失敗しました。";
+          }
+      }
 
-// 消去ボタンが押された場合の処理
-if (isset($_POST['delete'])) {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
+      // 消去ボタンが押された場合の処理
+      if (isset($_POST['delete'])) {
+          $name = $_POST['name'];
+          $phone = $_POST['phone'];
 
-    // プリペアドステートメントの準備
-    $stmt = $db->prepare("DELETE FROM arubaito_table WHERE 名前=? AND 電話番号=?");
-    $stmt->bindParam(1, $name);
-    $stmt->bindParam(2, $phone);
+          // プリペアドステートメントの準備
+          $stmt = $db->prepare("DELETE FROM arubaito_table WHERE 名前=? AND 電話番号=?");
+          $stmt->bindParam(1, $name);
+          $stmt->bindParam(2, $phone);
 
-    // プリペアドステートメントの実行
-    if ($stmt->execute()) {
-        echo "アルバイト情報が削除されました。";
-    } else {
-        echo "アルバイト情報の削除に失敗しました。";
-    }
-}
-
-// データベース接続を閉じる
-//$db = null;
+          // プリペアドステートメントの実行
+          if ($stmt->execute([$name, $phone])) {
+              echo "アルバイト情報が削除されました。";
+          } else {
+              echo "アルバイト情報の削除に失敗しました。";
+          }
+      }
 
       // アルバイト情報の取得
       $sql = 'SELECT * FROM arubaito_table';
@@ -68,6 +65,9 @@ if (isset($_POST['delete'])) {
         $hourlyRate = htmlspecialchars($row['時給'], ENT_QUOTES, 'UTF-8');
         echo "<tr><td>{$name}</td><td>{$phone}</td><td>{$hourlyRate}</td></tr>";
       }
+
+      // データベース接続を閉じる
+      $db = null;
       ?>
 
     </table>
