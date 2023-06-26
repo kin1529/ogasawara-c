8bf9d2d58c7e5e6eef67643ee48dafb5e960395a
井野駿也 <s2142012PV@s.chibakoudai.jp>
2023-06-27 01:54:06 +0900

    a

--- htdocs/6.php
+++ htdocs/6.php
@@ -10,7 +10,7 @@
        <div style="text-align:center" >名前をセレクトボタンで選択し<br>
        電話番号を入力してください</p>
        <label>名前</label>
-       <input type="text" name="uname" placeholder="名前"><br>
+       <input type="text" name="name" placeholder="名前"><br>
 
        <label>電話番号</label>
        <input type="text" name="bangou" placeholder="電話番号"><br>
@@ -19,3 +19,69 @@
     </from>
 </body>
 </html>
+
+
+<?php
+// フォームが送信されたかどうかを確認
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // 入力データを取得
+    $userId = $_POST["name"];
+    $password = $_POST["bangou"];
+
+    // フォームのバリデーション
+    if (empty($userId) || empty($password)) {
+        // 必須フィールドが空の場合、エラーメッセージを表示して処理を中止
+        echo "ユーザーIDとパスワードを入力してください";
+        exit;
+    }
+
+    // データベース接続設定
+    require_once ("db.php");
+
+    $conn = new PDO("mysql:host=$dbSever;dbname=$dbName", $dbUser, $dbPass);
+
+    // ユーザーIDに対応するデータを取得
+    $query = "SELECT password FROM arubaito_table WHERE 名前=? AND 電話番号=?";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":名前", $userId);
+    $stmt->execute();
+
+    // パスワードの照合
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $storedPassword = $row["電話番号"];
+
+        if (password_verify($password, $storedPassword)) {
+            // ログイン成功の処理
+            echo "ログインに成功しました";
+             // ログイン成功の処理
+            header("Location: 8.php");
+            exit;
+
+            
+        } else {
+            // パスワードが一致しない場合の処理
+            echo "ユーザーIDまたはパスワードが間違っています";
+            // ログイン画面に戻るためのリンクを表示
+             '>a href="6.php">ログイン画面に戻る</a>';
+         exit;
+        }
+    } else {
+        // ユーザーIDが存在しない場合の処理
+        echo "ユーザーIDまたはパスワードが間違っています";
+        // ログイン画面に戻るためのリンクを表示
+         >'a href="6.php">ログイン画面に戻る</a>';
+     exit;
+    }
+
+
+    // データベース接続を閉じる
+    $conn = null;
+
+
+
+}
+
+
+
+?>
