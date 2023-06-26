887dc001257ea4c1aea35d56b002eef63ede7a22
井野駿也 <s2142012PV@s.chibakoudai.jp>
2023-06-27 00:31:21 +0900

    a

--- htdocs/6.php
+++ htdocs/6.php
@@ -10,10 +10,10 @@
        <div style="text-align:center" >名前をセレクトボタンで選択し<br>
        電話番号を入力してください</p>
        <label>名前</label>
-       <input type="text" name="uname" placeholder="名前"><br>
+       <input type="text" name="name" placeholder="名前"><br>
 
        <label>電話番号</label>
-       <input type="text" name="bangou" placeholder="電話番号"><br>
+       <input type="text" name="phone" placeholder="電話番号"><br>
 
        <button type="submit">ログイン</button>
     </from>
@@ -22,46 +22,34 @@
 
 <?php
 
+if (isset($_POST['register'])) {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $hourlyRate = $_POST['hourly-rate'];
+
 // データベース接続設定
 require_once ("db.php");
 
+try {
+    $dbh = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    $msg = $e->getMessage();
+}
 
-// ログインボタンが押された時の処理
-if (isset($_POST['login'])) {
-    // 入力枠に空が無いことをチェック
-    if($_POST['名前'] == "" || $_POST['電話番号'] == "") {
-        $_SESSION['index_err_msg'] = "ID・パスワードを入力してからログインボタンを押して下さい";
-        header("Location: ".$_SERVER['HTTP_REFERER']);  
-    }else{
-        try {
-            // データベースへの接続
-            $dsn = 'mysql:dbname=music_archive;host=127.0.0.1';
-            $dbh = new PDO($dsn, 'db_admin', 'admin');
-
-            // 入力されたIDのパスワード取得
-            $sql = 'SELECT 電話番号 FROM arubaito_table WHERE 名前 = :名前'; // SQL文を構成
-            $sth = $dbh->prepare($sql); // SQL文を実行変数へ投入
-            $sth->bindParam(':名前', $_POST['名前']); // ユーザIDを実行変数に挿入
-            $sth->execute(); // SQLの実行
-            $user_pass = $sth->fetch(); // 処理結果の取得
-            
-            // ログイン認証処理
-            if($user_pass!=0 && password_verify($_POST['電話番号'], $user_pass['電話番号'])) {
-                // ログイン成功時の処理
-                $_SESSION['名前'] = $_POST['名前']; // ログインIDを格納したセッション変数を定義
-                $_SESSION['index_err_msg'] = ""; // エラーメッセージの削除
-                header("Location:recorder.php");
-                }else{
-                    // ログイン失敗時にエラーメッセージを表示する処理
-                    $_SESSION['6_err_msg'] = "ユーザIDまたはパスワードに不備があります";
-                    header("Location: ".$_SERVER['HTTP_REFERER']);
-                }
-
-        // データベースへの接続に失敗した場合
-        } catch (PDOException $e) {
-            print('データベースへの接続　に失敗しました:' . $e->getMessage());
-        die();
-        }
-    }
+$sql = "SELECT * FROM arubaito_table WHERE 名前  = :name";
+$stmt = $dbh->prepare($sql);
+$stmt->bindValue(':name', $mail);
+$stmt->execute();
+$member = $stmt->fetch();
+//指定したハッシュがパスワードにマッチしているかチェック
+if (password_verify($_POST['phone'], $member['phone'])) {
+    //DBのユーザー情報をセッションに保存
+    $_SESSION['name'] = $member['name'];
+    $msg = 'ログインしました。';
+    $link = '<a href="8.php">ホーム</a>';
+} else {
+    $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    $link = '<a href="6.php">戻る</a>';
+}
 }
 ?>
