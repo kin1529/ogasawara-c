73932315d3366ff7fd9cbd3c0221d0ba69e20aa1
sekisakura <s2142058.kj@s.chibakoudai.jp>
2023-06-27 14:41:40 +0900

    ok

--- htdocs/6.php
+++ htdocs/6.php
@@ -3,21 +3,23 @@
     <head>
         <title>アルバイト画面</title>
         <link rel="stylesheet" type="text/css" href="6.css">
-</head>
-<body>
-    <form action="" method="post">
-       <h2>アルバイト個人情報ログイン</h2>
-       <div style="text-align:center" >名前をセレクトボタンで選択し<br>
-       電話番号を入力してください</p>
-       <label>名前</label>
-       <input type="text" name="name" placeholder="名前"><br>
+    </head>
+    <body>
+        <form action="" method="post">
+            <h2>アルバイト個人情報ログイン</h2>
+            <div style="text-align:center">
+                <p>名前をセレクトボタンで選択し<br>
+                電話番号を入力してください</p>
+                <label>名前</label>
+                <input type="text" name="name" placeholder="名前"><br>
 
-       <label>電話番号</label>
-       <input type="text" name="bangou" placeholder="電話番号"><br>
+                <label>電話番号</label>
+                <input type="text" name="bangou" placeholder="電話番号"><br>
 
-       <button type="submit">ログイン</button>
-    </from>
-</body>
+                <button type="submit">ログイン</button>
+            </div>
+        </form>
+    </body>
 </html>
 
 <?php
@@ -35,50 +37,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // データベース接続設定
-    require_once ("db.php");
+    require_once("db.php");
 
+    // プリペアドステートメントの準備
+    $stmt = $db->prepare("SELECT * FROM arubaito_table WHERE 名前=? AND 電話番号=?");
+    $stmt->bindParam(1, $name);
+    $stmt->bindParam(2, $phone);
 
-     // プリペアドステートメントの準備
-     $stmt = $db->prepare("DELETE FROM arubaito_table WHERE 名前=? AND 電話番号=?");
-     $stmt->bindParam(1, $name);
-     $stmt->bindParam(2, $phone);
+    // プリペアドステートメントの実行
+    $stmt->execute();
 
-    // パスワードの照合
-    if ($stmt->rowCount() > 0) {
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $storedPassword = $row["電話番号"];
+    // 結果の取得
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if (password_verify($password, $storedPassword)) {
-            // ログイン成功の処理
-            echo "ログインに成功しました";
-             // ログイン成功の処理
-            header("Location:8.php");
-            exit;
-
-            
-        } else {
-            // パスワードが一致しない場合の処理
-            echo "ユーザーIDまたはパスワードが間違っています";
-            // ログイン画面に戻るためのリンクを表示
-            echo'<a href="'."6.php".'">'."ログイン画面に戻る".'</a>';
-         exit;
-        }
+    // ログインの判定と処理
+    if ($result) {
+        // ログイン成功の処理
+        echo "ログインに成功しました";
+        // ログイン成功の処理
+        header("Location: 8.php");
+        exit;
     } else {
-        // ユーザーIDが存在しない場合の処理
+        // ログイン失敗の処理
         echo "ユーザーIDまたはパスワードが間違っています";
         // ログイン画面に戻るためのリンクを表示
-        echo'<a href="'."6.php".'">'."ログイン画面に戻る".'</a>';
-     exit;
+        echo '<a href="6.php">ログイン画面に戻る</a>';
+        exit;
     }
-
 
     // データベース接続を閉じる
     $db = null;
-
-
-
 }
-
-
-
 ?>
